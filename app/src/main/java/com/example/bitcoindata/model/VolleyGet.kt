40a29f86b0f45d2e0245faeb8041666680b94d37
AndroidOS <burrowsgit@gmail.com>
2020-01-26 07:27:51 +1100

    Added OkHttpClient()

--- app/src/main/java/com/example/bitcoindata/model/VolleyGet.kt
+++ app/src/main/java/com/example/bitcoindata/model/VolleyGet.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.HttpHeaderParser
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
@@ -12,11 +13,13 @@ import org.json.JSONObject
 const val TAG = "VolleyGet"
 class VolleyGet(val context: Context) {
 
-    fun getPrices() {
+    var pricesParse = mutableListOf<Price>()
+
+    fun getPrices(): List<Price> {
         //Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(context)
         val url = "https://api.coindesk.com/v1/bpi/historical/close.json"
-        var data: String?
+
 
         // Request a string response from the provided URL.
         val stringRequest = StringRequest(
@@ -24,16 +27,24 @@ class VolleyGet(val context: Context) {
             Response.Listener<String> { response ->
                 // Display the first 500 characters of the response string.
                 parseJSON(response.toString())
-//                val bitcoinPrices = "Response is: ${response.substring(0, 500)}"
-//                //Log.d(TAG, "$bitcoinPrices")
+
             },
-            Response.ErrorListener { "That didn't work! " })
+            Response.ErrorListener { "That didn't work! "
+            })
+
+
+
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest)
+
+
+        return pricesParse
     }
 
     fun parseJSON(data: String){
+
+
 
         val jsonObject = JSONObject(data)
 
@@ -41,17 +52,21 @@ class VolleyGet(val context: Context) {
         val pricesKeys = prices.keys()
 
 
-        Log.d(TAG, "${prices}")
+       // Log.d(TAG, "${prices}")
 
         while (pricesKeys.hasNext()) {
             val key: String = pricesKeys.next()
 
             val price = prices[key] as Double
             Log.d(TAG, "${price}")
+            val newPrice = Price(key, price)
+            pricesParse.add(newPrice)
         }
 
 
-
-
     }
+
+
+
+
 }
--- app/src/main/java/com/example/bitcoindata/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/bitcoindata/viewmodel/ListViewModel.kt
@@ -5,10 +5,13 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.example.bitcoindata.model.Price
+import com.example.bitcoindata.model.VolleyGet
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import okhttp3.*
+import java.io.IOException
 
 private const val TAG = "ListViewModel"
 
@@ -16,7 +19,9 @@ class ListViewModel(application: Application) : BaseViewModel(application) {
 
 
 
-    private val disposable = CompositeDisposable()
+    private val fetch = VolleyGet(application)
+
+
 
 
 
@@ -30,11 +35,39 @@ class ListViewModel(application: Application) : BaseViewModel(application) {
 
     private fun fetchFromRemote() {
         loading.value = true
+//        Log.d(TAG, "ListViewModel ${fetch.getPrices()}")
+        Log.d(TAG, "ListViewModel ${fetchJson()}")
+
+
 
 
 
 
     }
+
+    fun fetchJson(){
+
+        val url = "https://api.coindesk.com/v1/bpi/historical/close.json"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("${e?.message}")
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                fetch.parseJSON(body.toString())
+
+                //editText?.text = "${body.toString()}" \\ or whatever else you wanna set on the edit text
+            }
+        })
+
+    }
+
 
 
     override fun onCleared() {
